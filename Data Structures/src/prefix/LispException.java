68612e1e9981ea565c02abaeb7598010b01e8238
17szabod <daniszabo99@gmail.com>
2016-09-24 16:01:29 -0500

    9/24 did assignment 3

---
+++ Data Structures/src/prefix/LispException.java
@@ -0,0 +1,15 @@
+package prefix;
+import java.*;
+
+public class LispException extends RuntimeException
+	{
+	public LispException()
+	{
+		super();
+	}
+
+	public LispException(String message)
+	{
+		super(message);
+	}
+}
