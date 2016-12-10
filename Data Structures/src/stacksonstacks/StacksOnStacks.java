2d8fd1716cb73ba68e0b8d636bae76a692cbb284
17szabod <daniszabo99@gmail.com>
2016-12-10 13:03:24 -0600

    Do I need to commit?

---
+++ Data Structures/src/stacksonstacks/StacksOnStacks.java
@@ -0,0 +1,33 @@
+package stacksonstacks;
+
+import java.util.Scanner;
+
+public class StacksOnStacks {
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = 0;
+		int[][] supArr = new int[n][10000];
+		for (int y = 0; y < m; y++) {
+			m = in.nextInt();
+			int[] arr = new int[m];
+			for (int x = 0; x < m; x++) {
+				arr[x] = in.nextInt();
+			}
+			supArr[y] = arr;
+		}
+		int c = 0;
+		int temp = 0;
+		for (int y = 0; y < supArr.length; y++) {
+			temp = supArr[y][0] % 2;
+			for (int x = 1; x < supArr[y].length; x++) {
+				int thisOne = supArr[y][x] % 2;
+				if (temp - thisOne == 0) {
+					c++;
+				}
+			}
+			System.out.println("Row #" + y + ": Jerry needs to add a minimum of " + c + "coins");
+		}
+	}
+
+}
