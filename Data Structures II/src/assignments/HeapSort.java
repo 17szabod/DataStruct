b1bb0fb5a07eb2156e331855ad3218897b50e5a9
Daniel Szabo <daniszabo99@gmail.com>
2017-03-11 14:55:49 -0600

    Update HeapSort.java

--- Data Structures II/src/assignments/HeapSort.java
+++ Data Structures II/src/assignments/HeapSort.java
@@ -1,24 +1,37 @@
 package assignments;
 
-public class HeapSort {
 
-	public static void sortHeap(int[] arr) {
+public class HeapSort {
+	/**
+	 * Sorts array arr in descending order
+	 * @param arr  array to be sorted
+	 * @return the heap array construction and the sorted list
+	 */
+	public static String sortHeap(int[] arr) {
+		String result = "";
 		int[] construct = new int[arr.length];
 		for (int x = 0; x < arr.length; x++) {
 			construct = constructAdd(construct, arr[x], x);
 		}
 		for (int i : construct) {
-			System.out.print(i + " ");
+			result += i + " ";
 		}
-		System.out.println();
+		result += "\n";
 		for (int x = 0; x < construct.length; x++) {
 			int currNode = construct[0];
-			System.out.print(currNode + " ");
+			result += currNode + " ";
 			int lastIndex = construct.length - x - 1;
 			construct = swap(0, lastIndex, construct);
 			construct = removeLast(construct, lastIndex);
 		}
+		return result;
 	}
+	/**
+	 * Removes the root element of the heap array, replacing it with the last element and then heapifying it
+	 * @param arr  the array from which to remove the element
+	 * @param lastIndex  the index of the last element in the array
+	 * @return the inputed array with root element removed in heap form
+	 */
 	private static int[] removeLast(int[] arr, int lastIndex) {
 		arr[lastIndex] = 0;
 		int y = 0;
@@ -52,7 +65,15 @@ public class HeapSort {
 		return arr;
 		
 	}
+	/**
+	 * Adds element i to position x in heapified array arr and then heapifies array
+	 * @param arr  the heap array which should be added to
+	 * @param i  the value being added
+	 * @param x  the index to be added at
+	 * @return  the heap adjusted array with new element
+	 */
 	private static int[] constructAdd(int[] arr, int i, int x) {
+		arr[x] = i;
 		boolean done = false;
 		int nextX = x;
 		while (!done) {
@@ -64,6 +85,13 @@ public class HeapSort {
 		}
 		return arr;
 	}
+	/**
+	 * Swaps elements at i and j in array arr
+	 * @param i  the index of one of the elements
+	 * @param j  the index of the other element
+	 * @param arr  the array in which to swap
+	 * @return  the array inputed with specified elements swapped
+	 */
 	private static int[] swap(int i, int j, int[] arr) {
 		int temp = arr[i];
 		arr[i] = arr[j];
@@ -71,7 +99,12 @@ public class HeapSort {
 		return arr;
 	}
 	public static void main(String[] args) {
-
+		//int[] arr = {10, 9, 4, 3, 7, 1, 5, 2, 8, 6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+		int[] arr = new int[10000];
+		for (int x = 0; x < 10000; x++) {
+			arr[x] = (int) (Math.random() * 100000);
+		}
+		System.out.println(sortHeap(arr));
 	}
 
 }
