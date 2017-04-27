615fa81397e21b073c032e9ad37e9b2130556e7d
Daniel Szabo <daniszabo99@gmail.com>
2017-04-26 21:16:32 -0500

    Update HuffmanTree.java

--- Data Structures II/src/assignments/HuffmanTree.java
+++ Data Structures II/src/assignments/HuffmanTree.java
@@ -2,17 +2,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-
+/**
+ * Creates and decodes Huffman tree from given file input
+ * @author daniel
+ *
+ */
 public class HuffmanTree {
 	private static PriorityQueue<HuffNode> queue;
 	private static HuffNode root;
+	
 	public static void main(String[] args) {
 		queue = new PriorityQueue<HuffNode>();
 		Scanner scan = null;
 		try {
-			scan = new Scanner(new FileReader("H:\\New Attempt\\DataStruct\\Data Structures\\Data Structures II\\src\\assignments\\Huffman.txt"));
+			//Could only find file for me when I put in the full path, might work differently on a different computer so
+			//path would probably have to be changed
+			scan = new Scanner(new FileReader("//home//daniel//workspace//Data Structures II//src//testCase2.txt"));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Change the filepath");
 			e.printStackTrace();
 		}
 		while (scan.hasNextLine()) {
@@ -20,14 +27,21 @@ public class HuffmanTree {
 			double freq = scan.nextDouble();
 			queue.add(new HuffNode(symbol, freq));
 		}
+		System.out.println("The Huffman code for the given characters is:");
 		decode();
 	}
+	/**
+	 * Creates a Huffman tree and then prints the code
+	 * Note printing is a side effect of this method
+	 */
 	private static void decode() {
 		recAdd(queue.poll(), queue.poll());
-		//print(root);
 		printLeaves(root, "");
 	}
-	
+	/**
+	 * A private method used for testing, thought I'd keep it in for kicks
+	 * @param tree the root node of the tree to print
+	 */
 	private static void print(HuffNode tree) {
 		System.out.println(tree.getValue() + " " + tree.getFreq());
 		if (tree.getLeft() != null)
@@ -35,11 +49,15 @@ public class HuffmanTree {
 		if (tree.getRight() != null)
 			print((HuffNode) tree.getRight());
 	}
-	
+	/**
+	 * Prints the codes of every leaf in a Huffman tree
+	 * @param tree the root node of the tree to print the leaves of
+	 * @param toPrint a String to hold the path to the root
+	 */
 	private static void printLeaves(HuffNode tree, String toPrint) {
 		if (tree != null) {
 			  if (tree.getLeft() == null && tree.getRight() == null) {
-				  System.out.println(toPrint + " " + tree.getValue());
+				  System.out.println(tree.getValue() + ": " + toPrint);
 			  }
 			  if (tree.getLeft() != null) {
 				  printLeaves((HuffNode) tree.getLeft(), toPrint + "0");
@@ -49,17 +67,20 @@ public class HuffmanTree {
 			  }
 		  }
 	}
-	
-	private static boolean recAdd(HuffNode left, HuffNode right) {
+	/**
+	 * Recursively adds nodes from queue together to make a Huffman tree
+	 * @param left the node with the smaller frequency
+	 * @param right the node with the larger frequency
+	 */
+	private static void recAdd(HuffNode left, HuffNode right) {
 		HuffNode parent = new HuffNode("NUM_VAL", right.getFreq() + left.getFreq());
 		parent.setLeft(left);
 		parent.setRight(right);
 		if (queue.isEmpty()) {
 			root = parent;
-			if (root.getFreq() != 1.0) System.out.println("oh noes");
-			return true;
+			return;
 		}
 		queue.add(parent);
-		return recAdd(queue.poll(), queue.poll());
+		recAdd(queue.poll(), queue.poll());
 	}
 }
